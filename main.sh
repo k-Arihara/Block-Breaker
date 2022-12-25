d829069e2468be0cedf5cafe1ec55470f1d78e1c
k-Arihara <dev9876dora@gmail.com>
2022-12-26 00:15:45 +0900

    [update]Use array in drow func

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
+# Environment Value
 FIELD="
 #################################
 #                               #
@@ -9,7 +10,7 @@ FIELD="
 #     *                         #
 #      @                        #
 #  @@@  @                       #
-#################################\033[9A\r"
+#################################"
 
 FIELD_WIDTH=33
 FIELD_HEIGHT=9
@@ -18,12 +19,22 @@ Y=2
 VX=1
 VY=1
 
+# Define Function
 getch() {
   old=$(stty -g)
   stty raw -echo min 0 time 1
   printf '%s' $(dd bs=1 count=1 2>/dev/null)
   stty $old
 }
+
+# Main Function
+
+FIELD_WITHOUT_CRLF=$(echo "$FIELD" | sed -e ':loop' -e 'N; $!b loop' -e 's/\n//g')
+for i in $(seq "${#FIELD_WITHOUT_CRLF}"); do
+  elem=$(printf "$FIELD_WITHOUT_CRLF" | cut -c "$i")
+  # echo $ELEM
+  FIELD_ARR+=("$elem")
+done
 
 pos=1
 while :; do
@@ -42,37 +53,41 @@ while :; do
   esac
   printf "$c\r"
 
-  NEXT_X_C=$(echo "$FIELD" | sed -e ':loop' -e 'N; $!b loop' -e 's/\n//g' | cut -c $((((Y-1)*FIELD_WIDTH)+X+VX)))
-  NEXT_Y_C=$(echo "$FIELD" | sed -e ':loop' -e 'N; $!b loop' -e 's/\n//g' | cut -c $(((Y+VY-1)*FIELD_WIDTH+X)))
-  NEXT_XY_C=$(echo "$FIELD" | sed -e ':loop' -e 'N; $!b loop' -e 's/\n//g' | cut -c $(((Y+VY-1)*FIELD_WIDTH+X+VX)))
-  printf '%s %s' "$NEXT_X_C" "$NEXT_Y_C"
-  # printf "XY:$((Y*FIELD_WIDTH+X)), X+VX:$(((Y*FIELD_WIDTH)+X+VX))), Y+VY:$(((Y+VY)*FIELD_WIDTH+X)))"
+  NEXT_X_C=${FIELD_ARR[$(((Y * FIELD_WIDTH) + X + VX))]}
+  NEXT_Y_C=${FIELD_ARR[$(((Y + VY) * FIELD_WIDTH + X))]}
+  NEXT_XY_C=${FIELD_ARR[$(((Y + VY) * FIELD_WIDTH + X + VX))]}
+  # printf '%s %s\n' "$NEXT_X_C" "$NEXT_Y_C"
 
-  if [ "$NEXT_X_C" = '#' ] || [ "$NEXT_X_C" = '@' ] || [ "$NEXT_Y_C" = '#' ] || [ "$NEXT_Y_C" = '@' ]; then
-    if [ "$NEXT_X_C" = '#' ] || [ "$NEXT_X_C" = '@' ]; then
-      VX=$((VX*-1))
+  if [[ '#@-' =~ "$NEXT_X_C" ]] || [[ '#@-' =~ "$NEXT_Y_C" ]]; then
+    if [[ '#@-' =~ "$NEXT_X_C" ]]; then
+      VX=$((VX * -1))
     fi
-    if [ "$NEXT_Y_C" = '#' ] || [ "$NEXT_Y_C" = '@' ]; then
-      VY=$((VY*-1))
+    if [[ '#@-' =~ "$NEXT_Y_C" ]]; then
+      VY=$((VY * -1))
     fi
-  elif [ "$NEXT_XY_C" = '#' ] || [ "$NEXT_XY_C" = '@' ];then
-    VX=$((VX*-1))
-    VY=$((VY*-1))
+  elif [[ '#@-' =~ "$NEXT_XY_C" ]]; then
+    VX=$((VX * -1))
+    VY=$((VY * -1))
   fi
 
-  printf "$FIELD"
-  printf "\033[$((${FIELD_HEIGHT}-2))B\033[${pos}C-\033[$((${FIELD_HEIGHT}-2))A\r"
-  printf "\033[$((Y))B\033[$((X-1))Co\033[$((Y))A\r"
+  ball_xy=$((Y*FIELD_WIDTH+X)) || true
+  FIELD_TMP=""
+  for i in $(seq 0 "${#FIELD_WITHOUT_CRLF}"); do
+    if [ $(("$i" % FIELD_WIDTH)) = 0 ]; then
+      FIELD_TMP="$FIELD_TMP\n"
+    fi
+    if [ $((6 * FIELD_WIDTH + pos)) = "$i" ]; then
+      FIELD_TMP="${FIELD_TMP}-"
+    elif [ $ball_xy = "$i" ];then
+      FIELD_TMP="${FIELD_TMP}o"
+    else
+      FIELD_TMP="${FIELD_TMP}${FIELD_ARR[$i]}"
+    fi
+  done
+  printf "$FIELD_TMP\033[10A\r"
 
-  X=$((X+VX))
-  Y=$((Y+VY))
+  X=$((X + VX))
+  Y=$((Y + VY))
 
-  # for i in $(seq $((FIELD_WIDTH-2)));do
-  #   if [ $i = $pos ];then
-  #     printf "-"
-  #   else
-  #     printf " "
-  #   fi
-  # done
   # sleep 0.25
 done
