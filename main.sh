d6bb48aeecb6cb8ee7caafd454e44aa53f265768
k-Arihara <dev9876dora@gmail.com>
2022-12-26 23:57:52 +0900

    [update]likely block breaker

--- main.sh
+++ main.sh
@@ -2,18 +2,24 @@
 
 # Environment Value
 FIELD="
-#################################
-#                               #
-#                               #
-#                               #
-#    *                          #
-#     *                         #
-#      @                        #
-#  @@@  @                       #
-#################################"
+#####################################
+#                                   #
+#                                   #
+#    @@@@@@@@@@@@@@@@@@@@@@@@@@@    #
+#    @@@@@@@@@@@@@@@@@@@@@@@@@@@    #
+#    @@@@@@@@@@@@@@@@@@@@@@@@@@@    #
+#    @@@@@@@@@@@@@@@@@@@@@@@@@@@    #
+#    @@@@@@@@@@@@@@@@@@@@@@@@@@@    #
+#                                   #
+#                                   #
+#                                   #
+#                                   #
+#                                   #
+#                                   #
+#####################################"
 
-FIELD_WIDTH=33
-FIELD_HEIGHT=9
+FIELD_WIDTH=37
+FIELD_HEIGHT=16
 X=3
 Y=2
 VX=1
@@ -28,11 +34,9 @@ getch() {
 }
 
 # Main Function
-
 FIELD_WITHOUT_CRLF=$(echo "$FIELD" | sed -e ':loop' -e 'N; $!b loop' -e 's/\n//g')
 for i in $(seq "${#FIELD_WITHOUT_CRLF}"); do
   elem=$(printf "$FIELD_WITHOUT_CRLF" | cut -c "$i")
-  # echo $ELEM
   FIELD_ARR+=("$elem")
 done
 
@@ -52,20 +56,31 @@ while :; do
   *) ;;
   esac
   printf "$c\r"
+  
+  # FIELD_ARR[$(((FIELD_HEIGHT-4) * FIELD_WIDTH + pos))]="-"
+  FIELD_ARR[$(((FIELD_HEIGHT-4) * FIELD_WIDTH + $X))]="-"
 
   NEXT_X_C=${FIELD_ARR[$(((Y * FIELD_WIDTH) + X + VX))]}
   NEXT_Y_C=${FIELD_ARR[$(((Y + VY) * FIELD_WIDTH + X))]}
   NEXT_XY_C=${FIELD_ARR[$(((Y + VY) * FIELD_WIDTH + X + VX))]}
-  # printf '%s %s\n' "$NEXT_X_C" "$NEXT_Y_C"
 
   if [[ '#@-' =~ "$NEXT_X_C" ]] || [[ '#@-' =~ "$NEXT_Y_C" ]]; then
     if [[ '#@-' =~ "$NEXT_X_C" ]]; then
+      if [[ "@" = "$NEXT_X_C" ]]; then
+        FIELD_ARR[$(((Y * FIELD_WIDTH) + X + VX))]=" "
+      fi
       VX=$((VX * -1))
     fi
     if [[ '#@-' =~ "$NEXT_Y_C" ]]; then
+      if [[ "@" = "$NEXT_Y_C" ]]; then
+        FIELD_ARR[$(((Y + VY) * FIELD_WIDTH + X))]=" "
+      fi
       VY=$((VY * -1))
     fi
   elif [[ '#@-' =~ "$NEXT_XY_C" ]]; then
+    if [[ "@" = "$NEXT_XY_C" ]]; then
+      FIELD_ARR[$(((Y + VY) * FIELD_WIDTH + X + VX))]=" "
+    fi
     VX=$((VX * -1))
     VY=$((VY * -1))
   fi
@@ -76,18 +91,18 @@ while :; do
     if [ $(("$i" % FIELD_WIDTH)) = 0 ]; then
       FIELD_TMP="$FIELD_TMP\n"
     fi
-    if [ $((6 * FIELD_WIDTH + pos)) = "$i" ]; then
-      FIELD_TMP="${FIELD_TMP}-"
-    elif [ $ball_xy = "$i" ];then
+    if [ $ball_xy = "$i" ];then
       FIELD_TMP="${FIELD_TMP}o"
     else
       FIELD_TMP="${FIELD_TMP}${FIELD_ARR[$i]}"
     fi
   done
-  printf "$FIELD_TMP\033[10A\r"
+  printf "$FIELD_TMP\033[${FIELD_HEIGHT}A\r"
+
+  # FIELD_ARR[$(((FIELD_HEIGHT-4) * FIELD_WIDTH + pos))]=" "
+  FIELD_ARR[$(((FIELD_HEIGHT-4) * FIELD_WIDTH + $X))]=" "
 
   X=$((X + VX))
   Y=$((Y + VY))
-
-  # sleep 0.25
+  # sleep 0.1
 done
